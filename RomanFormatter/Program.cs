bf2404abf879a22e14d678c1d552052a2284788f
Nicolas Fournel <nicholas.fournel@avanade.com>
2017-11-17 17:36:40 +0100

    Add project files.

---
+++ RomanFormatter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanFormatter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool go;
+            var romanFormatter = new RomanFormatter();
+            do
+            {
+                Console.WriteLine("Veuillez saisir un en entier à convertir en chiffres romains ou autre chose pour sortir");
+                go = int.TryParse(Console.ReadLine(), out int value);
+                if (go)
+                    Console.WriteLine(string.Format(romanFormatter, "Entier saisi:{0}, en chiffres romains: {0:Roman}", value));
+            }
+            while (go);
+        }
+    }
+}
---
+++ RomanFormatter/RomanFormatter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanFormatter
+{
+    /// <summary>
+    /// Sources
+    ///     https://fr.wikipedia.org/wiki/Num%C3%A9ration_romaine
+    ///     https://stackoverflow.com/questions/22392810/integer-to-roman-format
+    /// </summary>
+    class RomanFormatter : ICustomFormatter, IFormatProvider
+    {
+        private static List<Tuple<string, int>> table = new List<Tuple<string, int>>
+        {
+            new Tuple<string, int>("M",  1000),
+            new Tuple<string, int>("CM", 900),
+            new Tuple<string, int>("D",  500),
+            new Tuple<string, int>("CD", 400),
+            new Tuple<string, int>("C",  100),
+            new Tuple<string, int>("XC", 90),
+            new Tuple<string, int>("L",  50),
+            new Tuple<string, int>("XL", 40),
+            new Tuple<string, int>("X",  10),
+            new Tuple<string, int>("IX", 9),
+            new Tuple<string, int>("V",  5),
+            new Tuple<string, int>("IV", 4),
+            new Tuple<string, int>("I",  1),
+        };
+
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            if ((format == "Roman") && (arg.GetType() == typeof(int)))
+            {
+                var romanNumber = string.Empty;
+
+                var number = (int)arg;
+
+                while (number > 0)
+                {
+                    var element = table.First(n => n.Item2 <= number);
+                    number -= element.Item2;
+                    romanNumber += element.Item1;
+                }
+                return romanNumber;
+            }
+            else return HandleOtherFormats(format, arg);
+        }
+
+        private string HandleOtherFormats(string format, object arg)
+        {
+            if (arg is IFormattable)
+                return ((IFormattable)arg).ToString(format, CultureInfo.CurrentCulture);
+            else if (arg != null)
+                return arg.ToString();
+            else
+                return String.Empty;
+        }
+
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+                return this;
+            else
+                return null;
+        }
+    }
+}
